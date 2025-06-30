d1bfdc6e33bd2275aee1db50b067058f9b922fa4
PowerPride <directmonitor@gmail.com>
2025-06-30 19:34:27 +0500

    some code

--- src/main/java/model/Apple.java
+++ src/main/java/model/Apple.java
@@ -1,4 +1,19 @@
 package model;
 
-public class Apple {
+import model.constants.Discount;
+
+public class Apple extends Food{
+    protected String colour;
+    public Apple (int amount, double price){
+        super(amount, price, false);
+    }
+    //60% и все эти условия сюда впиливать чтоли, чет както не понятно
+    @Override
+    public Apple(String colour){
+        if (colour == "red"){
+            return ????;
+        } else {
+
+        }
+    }
 }
--- src/main/java/model/Food.java
+++ src/main/java/model/Food.java
@@ -4,4 +4,13 @@ public abstract class Food {
     protected int amount;
     protected double price;
     protected boolean isVegetarian;
+
+    public Food(int amount, double price, boolean isVegetarian) {
+        this.amount = amount;
+        this.price = price;
+        this.isVegetarian = isVegetarian;
+    }
+
+    protected Food() {
+    }
 }

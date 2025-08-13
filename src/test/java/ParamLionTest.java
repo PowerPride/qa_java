6e506deeaae6057190ce6f868703038835b16efd
PowerPride <directmonitor@gmail.com>
2025-08-13 18:05:11 +0500

    new test param

--- src/test/java/ParamLionTest.java
+++ src/test/java/ParamLionTest.java
@@ -1,12 +1,17 @@
 import com.example.Feline;
 import com.example.Lion;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 
 public class ParamLionTest {
 
+    @ParameterizedTest
+    @CsvSource({"Самец, true","Самка, false"})
+
+    public void doesHaveManeParamTest(String sex, boolean expected) throws Exception {
+        Lion lion = new Lion(sex, new Feline());
+        Assertions.assertEquals(expected, lion.doesHaveMane(), () -> ("Проверка для пола") + sex);
+    }
 }

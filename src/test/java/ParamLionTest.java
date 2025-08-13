260b19f21380f0c9499568ece857fd1fee4e5ce3
PowerPride <directmonitor@gmail.com>
2025-08-13 16:46:37 +0500

    new tests Lion

---
+++ src/test/java/ParamLionTest.java
@@ -0,0 +1,12 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+public class ParamLionTest {
+
+}
